266fba12137444a7826e84853b1a924f71ae0edb
Sara <kauri.lehtinen@metropolia.fi>
2017-04-10 20:20:53 +0300

    apiDoc added

--- public/main.js
+++ public/main.js
@@ -1,49 +1,103 @@
-const fetchJson = () => {
-
-    const myRequest = new Request('data.json');
-
-    const fetchData = fetch(myRequest).then((response) => {
-            return response.json();
+fetch(`http://localhost:3000/teams`, {method: 'GET'}).then((response) => {
+    return response.json();
 }).then((json) => {
-        console.log(json);
+    const dropDown = document.querySelector('#teamDropdown');
+    for (let item of json){
+        console.log(item.team);
+        const dropDownItem = `<a class="dropdown-item team-item" href="#" value=${item._id}>${item.team}</a>`;
+        dropDown.innerHTML += dropDownItem;
+    }
+    const dropDownItems = document.querySelectorAll('.team-item');
+    console.log(dropDownItems);
+    const title = document.querySelector('.title');
+    const stats = document.querySelector('.stats');
 
-        const title = document.querySelector('.title');
-        const stats = document.querySelector('.stats');
-        $('.dropdown-item').click(function() {
-            console.log( $(this).text() );
-            for (let item of json) {
+    for(let item of dropDownItems){
+        item.addEventListener('click', () => {
+            let team = item.innerHTML;
+            team = team.replace(' ', '%20');
+            console.log(team);
+            fetch(`http://localhost:3000/team/${team}`, {method: 'GET'}).then((response) => {
+                return response.json();
+            }).then((json) => {
+                title.innerHTML = "";
+                stats.innerHTML = "";
+                for (let key in json[0]) {
+                    if (json[0].hasOwnProperty(key)) {
 
-                if (item.team === $(this).text() ) {
-                    title.innerHTML = "";
-                    stats.innerHTML = "";
-                    for (let key in item) {
-                        if (item.hasOwnProperty(key)) {
+                        if (key === 'char'){
+                            const logo = document.querySelector('#team');
+                            logo.innerHTML = json[0][key];
+                        } else {
+                            if (key === '_id'){
 
-                            if (key === 'char'){
-                                const logo = document.querySelector('#team');
-                                logo.innerHTML = item[key];
-                            } else {
-                                console.log(key + " -> " + item[key]);
+                            }else {
+                                console.log(key + " -> " + json[0][key]);
                                 const titleElem = document.createElement('p');
                                 titleElem.innerHTML = key;
                                 title.appendChild(titleElem);
                                 const stat = document.createElement('p');
-                                stat.innerHTML = item[key];
+                                stat.innerHTML = json[0][key];
                                 stats.appendChild(stat);
                             }
                         }
                     }
                 }
-            }
+                console.log(json);
+            }).catch((e) => {
+                console.log(e);
+            });
         });
-
-
-
-
+    }
 
 }).catch((e) => {
-        console.log(e);
+    console.log(e);
 });
-};
 
-fetchJson();
+
+
+
+
+/*const title = document.querySelector('.title');
+const stats = document.querySelector('.stats');
+
+for(let item of dropDown){
+    item.addEventListener('click', () => {
+        let team = item.innerHTML;
+        team = team.replace(' ', '%20');
+        console.log(team);
+        fetch(`http://localhost:3000/team/${team}`, {method: 'GET'}).then((response) => {
+            return response.json();
+        }).then((json) => {
+            title.innerHTML = "";
+            stats.innerHTML = "";
+            for (let key in json[0]) {
+                if (json[0].hasOwnProperty(key)) {
+
+                    if (key === 'char'){
+                        const logo = document.querySelector('#team');
+                        logo.innerHTML = json[0][key];
+                    } else {
+                        if (key === '_id'){
+
+                        }else {
+                            console.log(key + " -> " + json[0][key]);
+                            const titleElem = document.createElement('p');
+                            titleElem.innerHTML = key;
+                            title.appendChild(titleElem);
+                            const stat = document.createElement('p');
+                            stat.innerHTML = json[0][key];
+                            stats.appendChild(stat);
+                        }
+                    }
+                }
+            }
+            console.log(json);
+        }).catch((e) => {
+            console.log(e);
+        });
+    });
+}*/
+
+
+
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
@@ -7,34 +7,276 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 mongoose.Promise = global.Promise; //ES6 Promise
 const Schema = mongoose.Schema;
-const router = express.Router();
 
 const dataSchema = new Schema({
-    name: String,
-    age: Number,
-    gender: {
-        type: String,
-        enum: ['male', 'female'],
-        default: 'male'
-    },
-    color: String,
-    weight: Number
+    team: {type: String,  required: true},
+    char: {type: String,  required: true},
+    gamesplayed: {type: Number,  required: true},
+    wins: {type: Number,  required: true},
+    loses: {type: Number,  required: true},
+    goalsfor: {type: Number,  required: true},
+    goalsagainst: {type: Number,  required: true},
+    shotsfor: {type: Number,  required: true},
+    shotsagainst: {type: Number,  required: true},
+    overtimes: {type: Number,  required: true},
+    overtimewins: {type: Number,  required: true},
+    overtimeloses: {type: Number,  required: true},
+    shootouts: {type: Number,  required: true},
+    shootoutwins: {type: Number,  required: true},
+    shootoutloses: {type: Number,  required: true},
+    homewins: {type: Number,  required: true},
+    homeloses: {type: Number,  required: true},
+    guestwins: {type: Number,  required: true},
+    guestloses: {type: Number,  required: true}
 });
 
-const Data = mongoose.model('cat', dataSchema);
+const Data = mongoose.model('nhl97', dataSchema);
+
+app.use(bodyParser.json());
+
+/**
+ * @apiDefine ExampleResult
+ * @apiSuccess {String} _id Unique team id
+ * @apiSuccess {String} team Name of the team
+ * @apiSuccess {Int} gamesplayed Games Played
+ * @apiSuccess {Int} wins Wins
+ * @apiSuccess {Int} loses Loses
+ * @apiSuccess {Int} goalsfor Goals for
+ * @apiSuccess {Int} goalsagainst Goals against
+ * @apiSuccess {Int} shotsfor Shots for
+ * @apiSuccess {Int} shotsagainst Shots against
+ * @apiSuccess {Int} overtimes Overtimes
+ * @apiSuccess {Int} overtimewins Overtime wins
+ * @apiSuccess {Int} overtimeloses Overtime loses
+ * @apiSuccess {Int} shootouts Shootouts
+ * @apiSuccess {Int} shootoutwins Shootout wins
+ * @apiSuccess {Int} shootoutloses Shootout loses
+ * @apiSuccess {Int} homewins Home wins
+ * @apiSuccess {Int} homeloses Home loses
+ * @apiSuccess {Int} guestwins Guest wins
+ * @apiSuccess {Int} guestloses Guest loses
+ */
+
+/**
+ * @api {post} /addteam Add new team
+ * @apiName PostTeam
+ * @apiGroup Teams
+ * @apiUse ExampleResult
+ * @apiSuccessExample Example data on success:
+ * {
+    "_id" : ObjectId("58e771f3fe906d397a64859b"),
+    "team" : "florida",
+    "char" : "l",
+    "gamesplayed" : 0,
+    "wins" : 0,
+    "loses" : 0,
+    "goalsfor" : 0,
+    "goalsagainst" : 0,
+    "shotsfor" : 0,
+    "shotsagainst" : 0,
+    "overtimes" : 0,
+    "overtimewins" : 0,
+    "overtimeloses" : 0,
+    "shootouts" : 0,
+    "shootoutwins" : 0,
+    "shootoutloses" : 0,
+    "homewins" : 0,
+    "homeloses" : 0,
+    "guestwins" : 0,
+    "guestloses" : 0
+}
+ *@apiError Err Error defining missing value is send in response
+ */
+
+
+app.post('/addteam', bodyParser.urlencoded({extend: true}), (req, res) => {
+    const newteam = new Data(req.body);
+    newteam.save(function (err) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else {
+            console.log('New team added');
+            res.send(204);
+        }
+    });
+});
+
+/**
+ * @api {get} /team/:team Get one Team
+ * @apiName GetTeam
+ * @apiParam {String} team Name of the desired team.
+ * @apiGroup Teams
+ * @apiUse ExampleResult
+ * @apiSuccessExample Example data on success:
+ * {
+    "_id" : ObjectId("58e771f3fe906d397a64859b"),
+    "team" : "florida",
+    "char" : "l",
+    "gamesplayed" : 0,
+    "wins" : 0,
+    "loses" : 0,
+    "goalsfor" : 0,
+    "goalsagainst" : 0,
+    "shotsfor" : 0,
+    "shotsagainst" : 0,
+    "overtimes" : 0,
+    "overtimewins" : 0,
+    "overtimeloses" : 0,
+    "shootouts" : 0,
+    "shootoutwins" : 0,
+    "shootoutloses" : 0,
+    "homewins" : 0,
+    "homeloses" : 0,
+    "guestwins" : 0,
+    "guestloses" : 0,
+    "__v" : 0
+}
+ *@apiError Error String 'No team named <code>team</code> found' send in response
+ */
+
+app.get('/team/:team', (req, res) => {
+    Data.find().where({'team' : req.params.team}).exec().then((team) => {
+        if(team.length === 0){
+             res.send('No team named ' + req.params.team + ' found');
+        }else {
+            res.send(team);
+        }
+    });
+});
+
+/**
+ * @api {get} /teams Get all teams
+ * @apiName GetTeams
+ * @apiGroup Teams
+ * @apiSuccess {JSON} Teams All teams from database received
+ * @apiSuccessExample Example data on success:
+ * [{
+    "_id" : ObjectId("58e771f3fe906d397a64859b"),
+    "team" : "florida",
+    "char" : "l",
+    "gamesplayed" : 0,
+    "wins" : 0,
+    "loses" : 0,
+    "goalsfor" : 0,
+    "goalsagainst" : 0,
+    "shotsfor" : 0,
+    "shotsagainst" : 0,
+    "overtimes" : 0,
+    "overtimewins" : 0,
+    "overtimeloses" : 0,
+    "shootouts" : 0,
+    "shootoutwins" : 0,
+    "shootoutloses" : 0,
+    "homewins" : 0,
+    "homeloses" : 0,
+    "guestwins" : 0,
+    "guestloses" : 0,
+    "__v" : 0
+},{
+    "_id" : ObjectId("58e771f3fe906d397a64859b"),
+    "team" : "New York",
+    "char" : "l",
+    "gamesplayed" : 0,
+    "wins" : 0,
+    "loses" : 0,
+    "goalsfor" : 0,
+    "goalsagainst" : 0,
+    "shotsfor" : 0,
+    "shotsagainst" : 0,
+    "overtimes" : 0,
+    "overtimewins" : 0,
+    "overtimeloses" : 0,
+    "shootouts" : 0,
+    "shootoutwins" : 0,
+    "shootoutloses" : 0,
+    "homewins" : 0,
+    "homeloses" : 0,
+    "guestwins" : 0,
+    "guestloses" : 0,
+    "__v" : 0
+}]
+ *@apiError Error String 'No teams found' send in response
+ */
+
+app.get('/teams', (req, res) => {
+    Data.find().exec().then((teams) => {
+        if(teams.length === 0){
+            res.send('no teams found');
+        }else {
+            res.send(teams);
+        }
+    });
+});
+
+/**
+ * @api {get} /morewinsthan/:wins Get all teams based on amount of winnings
+ * @apiName GetTeamsBasedOnWinnings
+ * @apiGroup Teams
+ * @apiParam {Number} wins Lower limit of winnings. Results with more than this will be received in response
+ * @apiUse ExampleResult
+ * @apiSuccessExample Example data on success:
+ * [{
+    "_id" : ObjectId("58e771f3fe906d397a64859b"),
+    "team" : "florida",
+    "char" : "l",
+    "gamesplayed" : 0,
+    "wins" : 8,
+    "loses" : 0,
+    "goalsfor" : 0,
+    "goalsagainst" : 0,
+    "shotsfor" : 0,
+    "shotsagainst" : 0,
+    "overtimes" : 0,
+    "overtimewins" : 0,
+    "overtimeloses" : 0,
+    "shootouts" : 0,
+    "shootoutwins" : 0,
+    "shootoutloses" : 0,
+    "homewins" : 0,
+    "homeloses" : 0,
+    "guestwins" : 0,
+    "guestloses" : 0,
+    "__v" : 0
+},{
+    "_id" : ObjectId("58e771f3fe906d397a64859b"),
+    "team" : "New York",
+    "char" : "l",
+    "gamesplayed" : 0,
+    "wins" : 5,
+    "loses" : 0,
+    "goalsfor" : 0,
+    "goalsagainst" : 0,
+    "shotsfor" : 0,
+    "shotsagainst" : 0,
+    "overtimes" : 0,
+    "overtimewins" : 0,
+    "overtimeloses" : 0,
+    "shootouts" : 0,
+    "shootoutwins" : 0,
+    "shootoutloses" : 0,
+    "homewins" : 0,
+    "homeloses" : 0,
+    "guestwins" : 0,
+    "guestloses" : 0,
+    "__v" : 0
+}]
+ *@apiError Error String 'no teams with more wins than <code>wins</code> found' send in response
+ */
+
+app.get('/morewinsthan/:wins', (req, res) => {
+    Data.find().where('wins').gt(req.params.wins).exec().then((teams) => {
+        if(teams.length === 0){
+            res.send('no teams with more wins than ' + req.params.wins + ' found');
+        }else {
+            res.send(teams);
+        }
+    });
+});
 
 mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB}`).then(() => {
-    console.log('Connected successfully youuu.');
-    Data.find().exec().then((cats) => {
-        console.log(`Got ${cats.length} cats`);
-    });
-    app.get('/', (req, res) => {
-        res.send('Hello Worldsadasdas!');
-    });
+    console.log('Mongodb connected successfully.');
     app.use(express.static(path.join(__dirname, 'public')));
-    app.on('listening',() => {
-        console.log('ok, server is running');
-    });
     app.listen(process.env.APP_PORT);
 }, err => {
     console.log('Connection to db failed: ' + err);
